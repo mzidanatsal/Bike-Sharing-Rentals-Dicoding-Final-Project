3195c56ae6e615c0fce3e8e086019994b04b3fb6
mzidanatsal <mzatsal19@gmail.com>
2024-10-03 22:22:24 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -298,4 +298,4 @@ axes[2].tick_params(axis='y', labelsize=10)
 plt.tight_layout()
 st.pyplot(fig)
 
-st.caption('Copyright (c) Muhammad Zidan Atsal 2023')
+st.caption('Copyright (c) Muhammad Zidan Atsal 2024')
